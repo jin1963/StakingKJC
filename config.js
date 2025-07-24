065c59b953e0ff8e767f442ee4ee9256e1a7ed98
jin1963 <kaojino89@gmail.com>
2025-07-25 00:48:26 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+const contractAddress = "0x99734D918a15E0Ac17519ffe20c3396fe7b2904B"; // G3X24 Staking Contract Address
+const tokenAddress = "0xCcf14757654ddeF0eC30ab117C4Cf3aEbF3b5604"; // G3X24 Token Address
+const chainId = 56; // Binance Smart Chain Mainnet
